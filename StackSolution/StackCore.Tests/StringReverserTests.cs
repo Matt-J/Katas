f1f2a695f4e6db694b0a0e7ef25e2f0b223b354f
matt.johnston <matt.johnston@15below.com>
2019-11-21 09:31:51 +0000

    Adding string reversers

---
+++ StackSolution/StackCore.Tests/StringReverserTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StackCore.Tests
+{
+    [TestFixture]
+    public class StringReverserTests
+    {
+        [Test]
+        public void ReverseShouldReturnReversedString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StackSolution/StackCore/StringReverser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackCore
+{
+    public class StringReverser : IStringReverser
+    {
+        public string ReverseString(string stringToReverse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IStringReverser
+    {
+        string ReverseString(string stringToReverse);
+    }
+}
